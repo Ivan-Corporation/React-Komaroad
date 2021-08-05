d0b6d684657b5a3d0a8f1b2211e4c8105bcba4b2
Ivan <hammer.99.ivan@gmail.com>
2021-08-06 01:09:47 +0700

    LifeCycle

--- Practice/Fundamental Topics/right-part-of-tree/src/App.js
+++ Practice/Fundamental Topics/right-part-of-tree/src/App.js
@@ -1,11 +1,71 @@
 import './App.css';
 import LoginStatus from './LoginStatus';
 
+import React from 'react';
+
+// class App extends React.Component {
+//    constructor(props) {
+//       super(props);
+      
+//       this.state = {
+//          data: 0
+//       }
+//       this.setNewNumber = this.setNewNumber.bind(this)
+//    };
+//    setNewNumber() {
+//       this.setState({data: this.state.data + 1})
+//    }
+//    render() {
+//       return (
+//          <div className='App-header'>
+//             <button onClick = {this.setNewNumber}>ИНКРИМИНИРОВАТЬ componentWillUpdate</button>
+//             <Content myNumber = {this.state.data}></Content>
+//          </div>
+//       );
+//    }
+// }
+// class Content extends React.Component {
+//    componentWillMount() {
+//       console.log('Component WILL MOUNT!')
+//    }
+//    componentDidMount() {
+//       console.log('Component DID MOUNT!')
+//    }
+//    componentWillReceiveProps(newProps) {    
+//       console.log('Component WILL RECIEVE PROPS!')
+//    }
+//    shouldComponentUpdate(newProps, newState) {
+//       return true;
+//    }
+//    componentWillUpdate(nextProps, nextState) {
+//       console.log('Component WILL UPDATE!');
+//    }
+//    componentDidUpdate(prevProps, prevState) {
+//       console.log('Component DID UPDATE!')
+//    }
+//    componentWillUnmount() {
+//       console.log('Component WILL UNMOUNT!')
+//    }
+//    render() {
+//       return (
+//          <div>
+//             <h3>{this.props.myNumber}</h3>
+//          </div>
+//       );
+//    }
+// }
+
+
+
+
 
 function App() {
   
   return <div className='App-header'>
   <LoginStatus isLoggedIn={false}/>
+
+
+  
   </div> 
 }
 
--- Practice/Fundamental Topics/right-part-of-tree/src/index.js
+++ Practice/Fundamental Topics/right-part-of-tree/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// setTimeout(() => {
+//   ReactDOM.unmountComponentAtNode(document.getElementById('root'));}, 10000);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+
+  
 );
 
