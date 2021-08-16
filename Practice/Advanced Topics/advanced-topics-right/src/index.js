c05bf91a4014f4b32cb7582bb08ec288ad42f7e1
Ivan <hammer.99.ivan@gmail.com>
2021-08-16 13:00:43 +0700

    adv-topics-right

---
+++ Practice/Advanced Topics/advanced-topics-right/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import HOC from './HOC';
+import Portals from './Portals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    
+        {/* <HOC /> */}
+
+        {/* <Portals/> */}
+
+        <
+
+
+
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
